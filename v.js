23f4209ed8d40636f12884197fe9debe9cd8d500
Vennela <venni3377@gmail.com>
2025-11-16 12:45:27 +0530

    Add files via upload

---
+++ v.js
@@ -0,0 +1,121 @@
+// let name ="vennela"
+// let age =20
+// let student = true
+// let marks = 85
+
+// console.log(name,age,student,marks)
+// let hobby=["dancing","cricket","singing"]
+// console.log(hobby);
+// let person={
+//     name:"jasmine",age:30
+// };
+// console.log(person.age)
+
+// fullname="tony stark"
+// age=24;
+// price=99.99;
+// console.log(y);
+
+// leta= 10;
+// let b= 3;
+// console.log(a+b)
+// console.log(a%b)
+// console.log(a**b)
+
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+// leta = 2;
+// letb = 3.14;
+// letc = "tony"
+// letd = true;
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// console.log(typeof (d));
+
+//for(i=1;i<=10;i++){
+    //if(i%2!==0){
+    //    console.log(i);
+  //  }
+//}
+//const car ={car: "rolls royce",brand: "SVGAnimatedAngle",model: "SpeechRecognitionResult",color:["black","blue","white"]}
+//console.log(car.color[1])
+
+
+
+//function add(a,b){
+  //  return a+b;
+//}
+//console.log(add(5,3));
+
+
+
+//function greet(){
+  //  console.log("Hello,world");
+//}
+ //greet();
+
+
+let movies = ["Idiots" , "Dangal", "KabirSingh", "jawan", "aashique2"];
+
+function PrintMovies(arr){
+    for (let i = 0; i<arr.length; i++){
+
+    }
+}
+print(movies);
+
+
+
+<html>
+<head>
+    <title>prt software solutions</title>
+</head>
+<body>
+
+    <h2>software solutions</h2>
+
+    <form id="myForm">
+        <label>jasmine:</label>
+        <input type="text" id="jasmine" />
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <script>
+        document.getElementById("myForm").addEventListener("submit", function(event) {
+    
+            
+            let name = document.getElementById("jasmine").value;
+            
+            console.log("software solutions: " + jasmine);
+        });
+    </script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
